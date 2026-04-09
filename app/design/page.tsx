6ebf703deae7f932e2f42f3f90152ddf69cb0b5e
Aki Inoyatov <info@gravityelements.com>
2026-04-08 22:34:24 -0700

    coming soon pg updated

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
     <main className="min-h-screen bg-black px-6 pt-32 text-white">
+      
       <h1 className="text-4xl tracking-[0.2em]">Coming Soon</h1>
+
     </main>
   )
 }
